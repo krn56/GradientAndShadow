e75fdc3c609781c79bf16cb1156db568e16ee46e
Roman Krendyasov <krendyasov_rn@mail.ru>
2024-11-01 12:43:34 +0400

    Initial Commit

---
+++ GradientAndShadow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradientAndShadow
+//
+//  Created by Роман Крендясов on 01.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
